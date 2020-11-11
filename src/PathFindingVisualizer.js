06100c41e9f85eef294df2118f5b9f68fb18debe
Rishabh Sood <sood.rishabh@hotmail.com>
2020-11-11 19:57:18 +0530

    cleaning initial commit and adding basic file structure

---
+++ src/PathFindingVisualizer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PathFindingVisualizer extends Component {
+    render() {
+        return (
+            <div>
+                FooBAR
+            </div>
+        );
+    }
+}
+
+export default PathFindingVisualizer;
