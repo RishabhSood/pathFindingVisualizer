01e1cf2b5c025263875d2398183990949c1cd630
Rishabh Sood <sood.rishabh@hotmail.com>
2020-11-13 11:57:19 +0530

    more refactoring

--- src/Algorithms/algorithms.js
+++ src/Algorithms/algorithms.js
@@ -30,7 +30,7 @@ export function depthFirstSearch(grid, startNode, finishNode) {
     visitedNodesInOrder.push(unvisitedNodes.shift());
     let nodeCount = unvisitedNodes.length;
     let currentNode = visitedNodesInOrder[visitedNodesInOrder.length - 1];
-    let neighbors = getNeighbors(currentNode, grid);
+    let neighbors = getUnvisitedNeighbors(currentNode, grid);
     while (nodeCount--) {
         neighbors = neighbors.filter(neighbor => !neighbor.isWall);
         if (neighbors.length) {
@@ -42,14 +42,14 @@ export function depthFirstSearch(grid, startNode, finishNode) {
                 return visitedNodesInOrder;
             }
             currentNode = visitedNodesInOrder[visitedNodesInOrder.length - 1];
-            neighbors = getNeighbors(currentNode, grid);
+            neighbors = getUnvisitedNeighbors(currentNode, grid);
         }
         else {
             while (neighbors.length === 0) {
                 currentNode = currentNode.previousNode;
                 if (currentNode === null)
                     return visitedNodesInOrder;
-                neighbors = getNeighbors(currentNode, grid);
+                neighbors = getUnvisitedNeighbors(currentNode, grid);
             }
         }
     }
@@ -73,16 +73,6 @@ function updateUnvisitedNeighbors(node, grid) {
 }
 
 function getUnvisitedNeighbors(node, grid) {
-    const neighbors = [];
-    const { col, row } = node;
-    if (row > 0) neighbors.push(grid[row - 1][col]);
-    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
-    if (col > 0) neighbors.push(grid[row][col - 1]);
-    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-    return neighbors.filter(neighbor => !neighbor.isVisited);
-}
-
-function getNeighbors(node, grid) {
     const neighbors = [];
     const { col, row } = node;
     if (row > 0) neighbors.push(grid[row - 1][col]);
